cb98d54f36740f751acc83e26909892afc528498
ming <1398565099@qq.com>
2020-05-11 01:05:42 +0800

    update UploadWorker.java file
    Signed-off-by: ming <1398565099@qq.com>

---
+++ app/src/main/java/com/example/workmanager/worker/UploadWorker.java
@@ -0,0 +1,20 @@
+package com.example.workmanager.worker;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class UploadWorker extends Worker {
+
+    public UploadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return Result.success();
+    }
+}
